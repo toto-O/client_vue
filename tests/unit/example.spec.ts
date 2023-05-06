f1cffadb2d1e6ee2e0396f5d9883e52297612d47
toto-O <phacusr3tr4@icloud.com>
2023-05-06 15:51:43 +0900

    edit unit_test, when refactoring rename variable

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,18 +1,34 @@
 import { shallowMount } from "@vue/test-utils";
-// import HelloWorld from "@/components/HelloWorld.vue";
-// import CapturedVideo from "@/views/CapturedVideo.vue";
+// import CapturedButton from "@/components/Molecule/CapturedButton.vue";
 import ControlButton from "@/components/Atoms/ControlButton.vue";
+// mockとして扱われているファイルはダミーとして扱い、対象のファイルが無いとして扱う
+// jest.mock("@/components/Molecule/CapturedButton.vue");
 
 describe("ControlButton.vue", () => {
+  // beforeEach(() => {
+  //   jest.resetAllMocks();
+  // });
   it("ボタンラベルが正しく表示されている", () => {
     const label_name = "START";
     const wrapper = shallowMount(ControlButton, {
       props: { label_name },
     });
-    expect(wrapper.text()).toMatch(label_name);
-    // "RESET" || "STOP" || "START"
+    expect(wrapper.find("button").text()).toMatch(label_name);
   });
-  // 、ボタンをクリックすると関数が起動する
+  it("ボタンをクリックするとignition_function関数が1回発生する", async () => {
+    const wrapper = shallowMount(ControlButton);
+    await wrapper.find("button").trigger("click");
+    // function名が変わると、error
+    expect(wrapper.emitted().click).toBeTruthy();
+    expect(wrapper.emitted().click?.length).toBe(1);
+  });
+
+  // auto Mock
+  // it("ボタンをクリックすると「何らかの重たい処理」が一回実行されること", async () => {
+  //   const wrapper = shallowMount(ControlButton);
+  //   await wrapper.find("button").trigger("click");
+  //   expect(CapturedButton.captured_video_reset).toHaveBeenCalledTimes(1);
+  // });
 });
 
 // CapturedVideo
